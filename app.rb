667666272ae8922706cc57d47d00844db4b0e620
Donna Amos <donnacamos.developer@gmail.com>
2019-07-05 00:39:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-   get '/' do 
-     erb :index
-   end 
-   
-   post '/' do 
-     erb :display_puppy
-   end 
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
+
+    erb :display_puppy
+  end
 end
