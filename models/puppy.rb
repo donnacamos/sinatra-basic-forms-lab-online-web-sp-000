612fb848386724bf729aa5e5d1559e3e1e85e4bc
Donna Amos <donnacamos.developer@gmail.com>
2019-07-05 00:19:24 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,5 +2,8 @@ class Puppy
   attr_accessor :name :breed :months_old 
   
   def initialize(name, breed, months_old)
-    
+    @name = name 
+    @breed = breed 
+    @months_old = months_old 
+  end 
 end 
